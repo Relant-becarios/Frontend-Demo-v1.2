12aa1a4436f90c433e3793cb7d2091a716a99bf7
Relant-becarios <relantbecario@relant.com.mx>
2026-06-30 16:19:41 -0600

    update-db

--- src/api/inventory.ts
+++ src/api/inventory.ts
@@ -51,7 +51,7 @@ const obtenerTokenJWT = async (): Promise<string | null> => {
 
 export const fetchProductos = async (query = '', categoria = 'Todas'): Promise<Producto[]> => {
   try {
-    const url = `http://localhost:8080/api/productos`
+    const url = `https://prueba-de-backend-en-frontend.onrender.com/`
     const response = await fetch(url)
 
     if (!response.ok) throw new Error('Error de red al obtener productos')
